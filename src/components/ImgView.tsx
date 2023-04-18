423491212cb69764d2ea7c9e1b8831686fccda57
fatemah <fatemah.mh@gmail.com>
2023-04-18 10:40:23 +0200

    fix rotation error

--- src/components/ImgView.tsx
+++ src/components/ImgView.tsx
@@ -12,21 +12,34 @@ interface Props{
 const ImgView:React.FC<Props>=(props)=>{
     let src =props.img
     let rotation=props.rotation;
+    let style={}
+    if ([90,-90,-270, 270].includes(rotation)){
+        console.log("rotation",rotation)
+        style={
+          maxWidth:'77vh',
+        }
+    }
     return(
         <React.Fragment>
-            <Box  sx={{ paddingTop:1,paddingBottom:1,paddingRight:1}}>
-                <Image  
-                        src={src}
-                        alt="Main Img"
-                        width={500}
-                        height={560}
-                        style={{ maxHeight: '80vh',
-                                 width:'100%',
-                                 objectFit: 'scale-down',
-                                 transform: `rotate(${rotation}deg)`,
-                               }}
-                        priority
-                        />
+            <Box sx={{paddingTop:1,paddingBottom:1,paddingRight:1}}>
+                <Box  sx={{maxHeight: '77vh',width:'100%',justifyContent:'center',display:'flex',
+                                                        
+                }}>
+                    <Image  
+                            src={src}
+                            alt="Main Img"
+                            width={500}
+                            height={560}
+                            style={{ ...style,
+                                    maxHeight: '80vh',
+                                    transform: `rotate(${rotation}deg) `,
+                                    objectFit: 'scale-down',
+                                    width:"100%",
+                                }}
+                            
+                            priority
+                            />
+                            </Box>
             </Box>
         </React.Fragment>
     )
